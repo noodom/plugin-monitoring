bcc5e8dd2e7614d3ced50084452cacaa22ea583c
Loïc <loic@jeedom.com>
2017-01-24 13:58:34 +0100

    fix dep

--- resources/install.sh
+++ resources/install.sh
@@ -5,7 +5,7 @@ echo "*             Installation des dépendances             *"
 echo "********************************************************"
 apt-get update
 echo 50 > /tmp/dependancy_monitoring_in_progress
-apt-get install -y php5-snmp
+apt-get install --yes --force-yes php5-snmp
 echo 100 > /tmp/dependancy_monitoring_in_progress
 echo "********************************************************"
 echo "*             Installation terminée                    *"
