8ff5aaf3edfb2660ab6652164841788fdd0cd802
Loïc <loic@jeedom.com>
2017-06-26 15:50:13 +0200

    fix install on stretch

--- resources/install_apt.sh
+++ resources/install_apt.sh
@@ -9,7 +9,10 @@ echo "*             Installation des dépendances             *"
 echo "********************************************************"
 apt-get update
 echo 50 > ${PROGRESS_FILE}
-apt-get install --yes --force-yes php5-snmp
+apt-get install --yes --force-yes php-snmp
+if [ $? -ne 0 ]; then
+	apt-get install --yes --force-yes php5-snmp
+fi
 echo 75 > ${PROGRESS_FILE}
 service apache2 restart
 echo 100 > ${PROGRESS_FILE}
